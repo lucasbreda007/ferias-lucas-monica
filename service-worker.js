0c9a133a96c787bd23682718c1f447366991350a
lucasbreda007 <lucasbreda007@gmail.com>
2026-08-12 11:11:29 -0300

    Atualiza PWA para v5 e mantém atualização automática

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="ferias-lm-v4";
+const CACHE_NAME="ferias-lm-v5";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
